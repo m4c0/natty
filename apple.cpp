aab38507f8d2c7b6a458becbdbe21f5f5a2bfe1b
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-17 13:29:28 +0100

    apple: using RAII for resources

--- apple.cpp
+++ apple.cpp
@@ -1,13 +1,22 @@
 #pragma leco add_framework CoreGraphics
 #include <CoreGraphics/CoreGraphics.h>
 
+import hai;
+
+namespace {
+struct deleter {
+  void operator()(CGColorSpaceRef r) { CGColorSpaceRelease(r); }
+};
+} // namespace
+
 void boosh() {
   void * data = NULL;
   unsigned w = 1024;
   unsigned h = 1024;
 
-  CGColorSpaceRef color_space = CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
-  CGContextRef ctx = CGBitmapContextCreate(data, w, h, 8, w * 4, color_space, kCGImageAlphaPremultipliedLast);
+  hai::value_holder<CGColorSpaceRef, deleter> color_space{
+      CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB)};
 
-  CGColorSpaceRelease(color_space);
+  CGContextRef ctx = CGBitmapContextCreate(data, w, h, 8, w * 4, *color_space,
+                                           kCGImageAlphaPremultipliedLast);
 }
