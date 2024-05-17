4cc3a0c432167744bcd3cd2fb6b3462f959a1b93
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-17 14:55:58 +0100

    apple: adding a bunch of CoreText stuff

--- apple.cpp
+++ apple.cpp
@@ -1,17 +1,19 @@
+#pragma leco add_framework CoreFoundation
 #pragma leco add_framework CoreGraphics
-#pragma leco add_framework Foundation
+#pragma leco add_framework CoreText
+
 #include <CoreGraphics/CoreGraphics.h>
+#include <CoreText/CoreText.h>
 
 import hai;
 
 namespace {
-struct error {};
-
 struct deleter {
   void operator()(CGColorSpaceRef r) { CGColorSpaceRelease(r); }
   void operator()(CGContextRef r) { CGContextRelease(r); }
-  void operator()(CGFontRef r) { CGFontRelease(r); }
+  void operator()(CFTypeRef r) { CFRelease(r); }
 };
+template <typename T> using ref = hai::value_holder<T, deleter>;
 } // namespace
 
 void boosh() {
@@ -19,18 +21,19 @@ void boosh() {
   unsigned w = 1024;
   unsigned h = 1024;
 
-  hai::value_holder<CGFontRef, deleter> font{
-      CGFontCreateWithFontName(CFSTR("Helvetica"))};
-  if (!*font)
-    throw error{};
+  ref<CTFontRef> font{CTFontCreateWithName(CFSTR("Helvetica"), 12, nullptr)};
 
-  hai::value_holder<CGColorSpaceRef, deleter> colour_space{
+  ref<CGColorSpaceRef> colour_space{
       CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB)};
-  if (!*colour_space)
-    throw error{};
 
-  hai::value_holder<CGContextRef, deleter> ctx{CGBitmapContextCreate(
+  ref<CGContextRef> ctx{CGBitmapContextCreate(
       data, w, h, 8, w * 4, *colour_space, kCGImageAlphaPremultipliedLast)};
-  if (!*ctx)
-    throw error{};
+
+  ref<CFMutableDictionaryRef> attrs{
+      CFDictionaryCreateMutable(nullptr, 1, &kCFTypeDictionaryKeyCallBacks,
+                                &kCFTypeDictionaryValueCallBacks)};
+  CFDictionaryAddValue(*attrs, kCTFontAttributeName, *font);
+
+  ref<CFAttributedStringRef> attr_str{
+      CFAttributedStringCreate(nullptr, CFSTR("Olá!"), *attrs)};
 }
