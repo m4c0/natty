d10d5ce7dc419b1b5dba733baf23fac772d6875c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-17 17:07:55 +0100

    windows: bitmap creation/selection

--- windows.cpp
+++ windows.cpp
@@ -4,13 +4,23 @@
 #include <windows.h>
 
 import hai;
+import stubby;
 
 namespace {
 struct deleter {
-  void operator()(HDC hdc) { DeleteDC(hdc); }
+  void operator()(HDC h) { DeleteDC(h); }
+  void operator()(HGDIOBJ h) { DeleteObject(h); }
 };
 
 template <typename T> using ref = hai::value_holder<T, deleter>;
 } // namespace
 
-void boosh() { ref<HDC> dc{CreateCompatibleDC(GetDC(nullptr))}; }
+void boosh() {
+  unsigned w = 1024;
+  unsigned h = 1024;
+  hai::array<stbi::pixel> data{w * h};
+
+  ref<HDC> dc{CreateCompatibleDC(GetDC(nullptr))};
+  ref<HBITMAP> bmp{CreateBitmap(w, h, 1, 32, data.begin())};
+  SelectObject(*dc, *bmp);
+}
