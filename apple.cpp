b1f5263e9076a01de307e2134469f43dcb3433f7
Eduardo Costa <m4c0@users.noreply.github.com>
2024-05-17 13:36:50 +0100

    apple: load Helvetica as example

--- apple.cpp
+++ apple.cpp
@@ -1,4 +1,5 @@
 #pragma leco add_framework CoreGraphics
+#pragma leco add_framework Foundation
 #include <CoreGraphics/CoreGraphics.h>
 
 import hai;
@@ -9,13 +10,19 @@ struct error {};
 struct deleter {
   void operator()(CGColorSpaceRef r) { CGColorSpaceRelease(r); }
   void operator()(CGContextRef r) { CGContextRelease(r); }
+  void operator()(CGFontRef r) { CGFontRelease(r); }
 };
 } // namespace
 
 void boosh() {
-  void * data = NULL;
+  void *data = NULL;
   unsigned w = 1024;
   unsigned h = 1024;
+
+  hai::value_holder<CGFontRef, deleter> font{
+      CGFontCreateWithFontName(CFSTR("Helvetica"))};
+  if (!*font)
+    throw error{};
 
   hai::value_holder<CGColorSpaceRef, deleter> colour_space{
       CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB)};
