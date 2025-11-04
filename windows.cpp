7ce4077b00219fbe506d72086ddbca19324bbeb9
Eduardo Costa <m4c0@users.noreply.github.com>
2025-11-04 12:19:02 +0000

    windows: reformat

--- windows.cpp
+++ windows.cpp
@@ -12,82 +12,71 @@ import stubby;
 namespace {
   using hbitmap = hay<HBITMAP, nullptr, DeleteObject>;
   using hdc     = hay<HDC,     nullptr, DeleteDC>;
-
-struct deleter {
-  void operator()(HGDIOBJ h) { DeleteObject(h); }
-};
-
-template <typename T> using ref = hai::value_holder<T, deleter>;
-} // namespace
+}
 
 namespace natty {
-struct font : HFONT__ {};
-font_t create_font(const char *name, unsigned size){
-  auto res = CreateFont(size, 0, 0, 0, FW_DONTCARE, false, false, false,
-                        ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
-                        DEFAULT_QUALITY, DEFAULT_PITCH, name);
-  return font_t{static_cast<font *>(res), [](auto x) { DeleteObject(x); }};
-}
-
-struct surface {
-  hdc dc;
-  hbitmap bmp;
-  RECT rect;
-  hai::array<unsigned> data;
-};
-surface_t create_surface(unsigned w, unsigned h) {
-  auto s = new surface{
-      .dc{CreateCompatibleDC(GetDC(nullptr))},
-      .bmp{CreateCompatibleBitmap(GetDC(nullptr), w, h)},
-      .rect{
-          .left = 0,
-          .top = 0,
-          .right = static_cast<long>(w),
-          .bottom = static_cast<long>(h),
-      },
-      .data{w * h},
-  };
-  SelectObject(s->dc, static_cast<HBITMAP>(s->bmp));
-  SetBkColor(s->dc, RGB(0, 0, 0));
-  SetTextColor(s->dc, RGB(255, 255, 255));
-  return surface_t{s, [](auto x) { delete x; }};
-}
-
-void draw(const draw_params & p) {
-  HDC dc = (*p.surface)->dc;
-  SelectObject(dc, *p.font);
-
-  RECT rect = (*p.surface)->rect;
-  rect.top = p.position.x;
-  rect.left = p.position.y;
-
-  hai::array<wchar_t> buf{static_cast<unsigned>(p.text.size())};
-  MultiByteToWideChar(CP_UTF8, 0, p.text.data(), p.text.size(), buf.begin(), buf.size());
-
-  DrawTextW(dc, buf.begin(), buf.size(), &rect, DT_SINGLELINE);
-}
-
-const hai::array<unsigned> &surface_data(surface *s) {
-  HDC dc = s->dc;
-  HBITMAP bmp = s->bmp;
-
-  auto w = s->rect.right - s->rect.left;
-  auto h = s->rect.bottom - s->rect.top;
-
-  BITMAPINFOHEADER bmi{};
-  bmi.biSize = sizeof(BITMAPINFOHEADER);
-  bmi.biWidth = w;
-  bmi.biHeight = -h; // Secret API trick: negative flips Y
-  bmi.biPlanes = 1;
-  bmi.biBitCount = 32;
-  bmi.biCompression = BI_RGB;
-
-  GetDIBits(dc, bmp, 0, h, s->data.begin(),
-            reinterpret_cast<BITMAPINFO *>(&bmi), DIB_RGB_COLORS);
-
-  for (auto &pix : s->data) {
-    pix |= 0xFFU << 24; // add alpha channel
+  struct font : HFONT__ {};
+  font_t create_font(const char *name, unsigned size){
+    auto res = CreateFont(size, 0, 0, 0, FW_DONTCARE, false, false, false,
+        ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+        DEFAULT_QUALITY, DEFAULT_PITCH, name);
+    return font_t { static_cast<font *>(res), [](auto x) { DeleteObject(x); } };
   }
-  return s->data;
-}
+
+  struct surface {
+    hdc dc;
+    hbitmap bmp;
+    RECT rect;
+    hai::array<unsigned> data;
+  };
+  surface_t create_surface(unsigned w, unsigned h) {
+    auto s = new surface {
+      .dc { CreateCompatibleDC(GetDC(nullptr)) },
+      .bmp { CreateCompatibleBitmap(GetDC(nullptr), w, h) },
+      .rect {
+        .left = 0,
+        .top = 0,
+        .right = static_cast<long>(w),
+        .bottom = static_cast<long>(h),
+      },
+      .data { w * h },
+    };
+    SelectObject(s->dc, static_cast<HBITMAP>(s->bmp));
+    SetBkColor(s->dc, RGB(0, 0, 0));
+    SetTextColor(s->dc, RGB(255, 255, 255));
+    return surface_t{s, [](auto x) { delete x; }};
+  }
+
+  void draw(const draw_params & p) {
+    HDC dc = (*p.surface)->dc;
+    SelectObject(dc, *p.font);
+
+    RECT rect = (*p.surface)->rect;
+    rect.top = p.position.x;
+    rect.left = p.position.y;
+
+    hai::array<wchar_t> buf { static_cast<unsigned>(p.text.size()) };
+    MultiByteToWideChar(CP_UTF8, 0, p.text.data(), p.text.size(), buf.begin(), buf.size());
+
+    DrawTextW(dc, buf.begin(), buf.size(), &rect, DT_SINGLELINE);
+  }
+
+  const hai::array<unsigned> & surface_data(surface * s) {
+    auto w = s->rect.right - s->rect.left;
+    auto h = s->rect.bottom - s->rect.top;
+
+    BITMAPINFOHEADER bmi{};
+    bmi.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.biWidth = w;
+    bmi.biHeight = -h; // Secret API trick: negative flips Y
+    bmi.biPlanes = 1;
+    bmi.biBitCount = 32;
+    bmi.biCompression = BI_RGB;
+
+    GetDIBits(s->dc, s->bmp, 0, h, s->data.begin(),
+        reinterpret_cast<BITMAPINFO *>(&bmi), DIB_RGB_COLORS);
+
+    for (auto &pix : s->data) pix |= 0xFFU << 24; // add alpha channel
+    return s->data;
+  }
 } // namespace natty
